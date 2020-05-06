0fd963ec6a4f0f2a5bb00d7f894421b3fdc7296a
Ryan Fay <rmfay@calpoly.edu>
2020-05-06 16:15:58 -0700

    made some fixes to the minor bugs

--- components/FavoritesView.js
+++ components/FavoritesView.js
@@ -1,91 +1,87 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
 import {
   SafeAreaView,
   TouchableOpacity,
   Button,
   FlatList,
   StyleSheet,
-  Text, Image, View,
-} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import Colors from '../constants/Colors';
-import Constants from 'expo-constants';
+  Text, Image, View
+} from 'react-native'
+import Constants from 'expo-constants'
 // import { useNavigation } from '@react-navigation/native';
-import { withNavigation } from 'react-navigation';
-import { useNavigation } from 'react-navigation-hooks';
+import { withNavigation } from 'react-navigation'
+import { useNavigation } from 'react-navigation-hooks'
 
 function Item({ image, name, rate, loc, diff, selected, onSelect, details, ...props }) {
-  const { navigate } = useNavigation();
+  const { navigate } = useNavigation()
 
   return (
     <TouchableOpacity
-     
+
       onPress={() => navigate('Hike')}
-      
+
       style={[
         styles.item,
-        { backgroundColor: selected ? '#bdb76b' : '#f0e68c' },
-      ]}
-    >
-    <View style= {{
+        { backgroundColor: selected ? '#bdb76b' : '#f0e68c' }
+      ]}>
+      <View style= {{
         flex: 1,
         flexDirection: 'column'
 
-    }}>
-        <Image 
-          source = {{uri: image}}
-          style = {{width: 200, height: 200, marginHorizontal: 10}}>
+      }}>
+        <Image
+          source = {{ uri: image }}
+          style = {{ width: 200, height: 200, marginHorizontal: 10 }}>
         </Image>
 
         <View style = {{
-            flex: 1,
-            flexDirection: 'column'
+          flex: 1,
+          flexDirection: 'column'
         }}>
 
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.name}>Location: {loc}</Text>
-            <Text style={styles.name}>Rating: {rate}</Text>
-            <Text style={styles.name}>Difficulty: {diff}</Text>
-            <Text style={styles.name}>Description: {details}</Text>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.name}>Location: {loc}</Text>
+          <Text style={styles.name}>Rating: {rate}</Text>
+          <Text style={styles.name}>Difficulty: {diff}</Text>
+          <Text style={styles.name}>Description: {details}</Text>
         </View>
 
-    </View>
+      </View>
     </TouchableOpacity>
-  );
+  )
 }
 
 class FavoritesView extends Component {
-  constructor(props) {
-      super(props);
-      
-    this.state = { 
-          selected: null,
-          isLoading: true,
-          hikes: []
-      };
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected: null,
+      isLoading: true,
+      hikes: []
+    }
 
-    this.getHikes = this.getHikes.bind(this);
+    this.getHikes = this.getHikes.bind(this)
   }
 
-  async getHikes() {
+  async getHikes () {
     let hikes = await axios.get('https://slo-explore-308.herokuapp.com/')
-     .then(res => res.data)
-     .then(data => {
-      this.setState({
-        selected: null,
-        isLoading: false,
-        hikes: data 
-      });
-      console.log(data);
-     });
+      .then(res => res.data)
+      .then(data => {
+        this.setState({
+          selected: null,
+          isLoading: false,
+          hikes: data
+        })
+        console.log(data)
+      })
   }
 
-  componentDidMount() {
-    let hikes = this.getHikes();
+  componentDidMount () {
+    let hikes = this.getHikes()
   }
 
-  render() {
+  render () {
     return (
       <SafeAreaView style={styles.container}>
         <Button
@@ -111,28 +107,24 @@ class FavoritesView extends Component {
           extraData={this.state.selected}
         />
       </SafeAreaView>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
+    marginTop: Constants.statusBarHeight
   },
   item: {
     backgroundColor: '#f0e68c',
     padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
   title: {
-    fontSize: 32,
-  },
-});
+    fontSize: 32
+  }
+})
 
 export default withNavigation(FavoritesView);
-
-
-
-
