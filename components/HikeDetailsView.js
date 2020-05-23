b646451b0ced241a6af13ba9e92fc0a7995f031c
ncpalmie <ncpalmie@calpoly.edu>
2020-05-22 22:30:42 -0700

    Restyled HikeDetails a bit more, enabled passing of paramters from ListView to HikeDetails so hikes now update depending on which list item was chosen

--- components/HikeDetailsView.js
+++ components/HikeDetailsView.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
+import StarRating from 'react-native-star-rating'
 import {
   ScrollView,
   StyleSheet,
-  Button,
   Image,
   Text, View
 } from 'react-native'
 import { withNavigation } from 'react-navigation'
-import { Divider, Icon, Card, registerCustomIconType } from 'react-native-elements'
+import { Divider, Card, registerCustomIconType } from 'react-native-elements'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 registerCustomIconType('font-awesome-5', FontAwesome5)
 
 class HikeDetailsView extends Component {
   render () {
+    const { navigation } = this.props
     return (
       <ScrollView style={styles.container}>
         <View style={styles.imageView}>
@@ -25,57 +26,30 @@ class HikeDetailsView extends Component {
           <Divider style={styles.divStyle}/>
         </View>
         <View>
-          <Card title={'The P'}>
+          <Card title={navigation.getParam('hikeName', 'Missing Hike Name')}>
             <View style={styles.infoView}>
-              <Icon
-                name='clock'
-                type='font-awesome-5'
-                size={22}
+              <Text style={styles.infoHeader}>Rating: </Text>
+              <StarRating
+                starSize={26}
+                rating={navigation.getParam('hikeRating', 0)}
+                fullStarColor='#4EF3AB'
               />
-              <Text style={styles.infoHeader}> Time: </Text>
             </View>
-            <View style={styles.infoView}>
-              <Icon
-                name='shoe-prints'
-                type='font-awesome-5'
-                size={18}
-              />
-              <Text style={styles.infoHeader}> Distance Hiked: </Text>
+            <View style={styles.descView}>
+              <Text style={styles.infoHeader}>Description: </Text>
+              <Text style={styles.descText}>{navigation.getParam('hikeDescription', 'No description')}</Text>
             </View>
-            <View style={styles.infoView}>
-              <Icon
-                name='hiking'
-                type='font-awesome-5'
-                size={22}
-              />
-              <Text style={styles.infoHeader}>  Current Pace: </Text>
-            </View>
-            <View style={styles.infoView}>
-              <Icon
-                name='mountain'
-                type='font-awesome-5'
-                size={18}
-              />
-              <Text style={styles.infoHeader}> Current Elevation: </Text>
+            <View style={styles.gearView}>
+              <Text style={styles.infoHeader}>Recommended Gear: </Text>
+              <Text style={styles.gearText}>{navigation.getParam('hikeGear', 'None')}</Text>
             </View>
           </Card>
         </View>
         <View style={styles.divView}>
           <Divider style={styles.divStyle}/>
         </View>
-        <Card title='Start/Stop Hike'>
-          <View style={styles.timerView}>
-            <View style={styles.buttonStyle}>
-              <Button
-                title='Start'
-              />
-            </View>
-            <View style={styles.buttonStyle}>
-              <Button
-                title='Stop'
-                color='#d96668'
-              />
-            </View>
+        <Card title='User Reviews'>
+          <View style={styles.reviewsView}>
           </View>
         </Card>
         <View style={{ marginTop: 40 }}>
@@ -95,6 +69,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#d6e9d7'
   },
+  descText: {
+    fontSize: 15
+  },
+  gearText: {
+    fontSize: 15
+  },
   imageStyle: {
     width: 360,
     height: 320
@@ -105,7 +85,8 @@ const styles = StyleSheet.create({
     marginTop: 5
   },
   infoHeader: {
-    fontSize: 20
+    fontSize: 20,
+    fontWeight: 'bold'
   },
   divView: {
     marginTop: 15,
@@ -116,7 +97,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'green',
     height: 2
   },
-  timerView: {
+  reviewsView: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
